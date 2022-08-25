0cee31ad4b52952a1e57492fae6f59f5528d5941
tjrohweder <tjrohweder@gmail.com>
2022-08-25 00:02:06 -0300

    Adding variables

--- main.tf
+++ main.tf
@@ -3,10 +3,62 @@ terraform {
   required_providers {
     aws = ">= 4"
   }
+
+  backend "s3" {
+    bucket = "tjrohweder-terraform-curso-state"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
-  profile             = "default"
-  shared_config_files = "~/.aws/credentials"
-  region              = "us-east-1"
+  profile                  = "default"
+  shared_credentials_files = ["~/.aws/credentials"]
+  region                   = var.region
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami                     = data.aws_ami.ubuntu.id
+  instance_type           = var.int_type
+  disable_api_termination = var.disable_api_termination
+
+  tags = {
+    Name = var.int_names[0]
+  }
+}
+
+resource "aws_instance" "web2" {
+  ami                     = data.aws_ami.ubuntu.id
+  instance_type           = var.int_type
+  disable_api_termination = var.disable_api_termination
+
+  tags = {
+    Name = var.int_names[1]
+  }
+}
+
+resource "aws_instance" "web3" {
+  ami                     = var.amis[var.region]
+  instance_type           = var.int_type
+  disable_api_termination = var.disable_api_termination
+
+  tags = {
+    Name = var.int_names[2]
+  }
 }
---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "int_type" {
+  description = "Tipo de instancia EC2 WEB"
+  default     = "t3.micro"
+}
+
+variable "disable_api_termination" {
+  description = "Protege a instancia contra delete acidental"
+  type        = bool
+  default     = false
+}
+
+variable "int_names" {
+  description = "Lista de nomes para instancias"
+  type        = list(string)
+  default     = ["Helloworld", "Helloworld2", "Helloworld3"]
+  #                   0               1             2
+}
+
+variable "amis" {
+  description = "Amis para determinadas regioes"
+  type        = map(any)
+  default = {
+    "us-east-1" = "ami-05fa00d4c63e32376"
+    "us-east-2" = "ami-0568773882d492fc8"
+  }
+}
